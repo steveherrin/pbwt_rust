56f7a9c48ece2a13afd21b3174f33e824dc5b9f9
Steve Herrin <steve.herrin@gmail.com>
2017-04-27 14:23:27 -0700

    Refactoring:
    - store alleles for a given site contiguous in memory to match access pattern
    - factor out getting a_{k+1} from a_k

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,50 @@
-fn build_prefix_array(haplotypes: &[u8], n_haplo: usize, n_sites: usize) -> Vec<usize> {
-    let mut prefix_order: Vec<usize> = vec![0; n_haplo];
+/// Returns the next positional prefix array (Durbin's a_{k+1}) from the current one
+///
+/// # Arguments
+///
+/// * `pos_prefix_array` - An array slice holding the current positional prefix array (a_k)
+/// * `haplotypes` - An array slice holding the haplotypes at the current (k-th) site
+fn next_positional_prefix_array(pos_prefix_array: &[usize], haplotypes: &[u8]) -> Vec<usize> {
+    let mut allele_0s: Vec<usize> = vec![];
+    let mut allele_1s: Vec<usize> = vec![];
+
+    for &i in pos_prefix_array {
+        if haplotypes[i] == 0 {
+            allele_0s.push(i);
+        } else {
+            allele_1s.push(i);
+        }
+    }
+    let mut next_pos_prefix_array: Vec<usize> = vec![];
+    next_pos_prefix_array.extend(&allele_0s);
+    next_pos_prefix_array.extend(&allele_1s);
+    next_pos_prefix_array
+}
+
+
+/// Returns the positional prefix array (Durbin's a_k)
+///
+/// # Arguments
+///
+/// * `haplotypes` - An array slice in which each row (of length n_haplo)
+///                  corresponds to a single site, and contains 0s and 1s
+///                  indicating allele
+/// * `n_haplo` - The number of haplotypes
+/// * `k_site` - The numbered site to build the array for
+fn build_positional_prefix_array(haplotypes: &[u8], n_haplo: usize, k_site: usize) -> Vec<usize> {
+    let mut pos_prefix_array: Vec<usize> = vec![0; n_haplo];
     for i in 0..n_haplo {
-        prefix_order[i] = i;
+        pos_prefix_array[i] = i;
     }
 
-    for k in 0..(n_sites-1) {
-        let mut allele_0s: Vec<usize> = vec![];
-        let mut allele_1s: Vec<usize> = vec![];
-        for i in &prefix_order {
-            if haplotypes[i*n_sites + k] == 0 {
-                allele_0s.push(*i);
-            } else {
-                allele_1s.push(*i);
-            }
-        }
-        prefix_order[0..allele_0s.len()].clone_from_slice(&allele_0s);
-        prefix_order[allele_0s.len()..n_haplo].clone_from_slice(&allele_1s);
+    for k in 0..(k_site-1) {
+        let haplotypes_at_k = &haplotypes[k*n_haplo..(k+1)*n_haplo];
+        pos_prefix_array = next_positional_prefix_array(&pos_prefix_array, &haplotypes_at_k);
     }
-    prefix_order
+    pos_prefix_array
 }
 
 fn main() {
-    let n_haplo: usize = 8; // M
-    let n_sites: usize = 6; // N
-    let haplotypes: Vec<u8> = vec![0, 1, 0, 1, 0, 1,
-                                   1, 1, 0, 0, 0, 1,
-                                   1, 1, 1, 1, 1, 1,
-                                   0, 1, 1, 1, 1, 0,
-                                   0, 0, 0, 0, 0, 0,
-                                   1, 0, 0, 0, 1, 0,
-                                   1, 1, 0, 0, 0, 1,
-                                   0, 1, 0, 1, 1, 0];
-    assert_eq!(n_haplo * n_sites, haplotypes.len());
-
-    let prefix_order = build_prefix_array(&haplotypes, n_haplo, n_sites);
-    println!("{:?}", prefix_order);
 }
 
 #[cfg(test)]
@@ -42,19 +52,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_prefix_array() {
+    fn test_positional_prefix_array() {
         let n_haplo: usize = 8;
         let n_sites: usize = 6;
-        let haplotypes: Vec<u8> = vec![0, 1, 0, 1, 0, 1,
-                                       1, 1, 0, 0, 0, 1,
-                                       1, 1, 1, 1, 1, 1,
-                                       0, 1, 1, 1, 1, 0,
-                                       0, 0, 0, 0, 0, 0,
-                                       1, 0, 0, 0, 1, 0,
-                                       1, 1, 0, 0, 0, 1,
-                                       0, 1, 0, 1, 1, 0];
-        let expected: Vec<usize> = vec![4, 1, 6, 0, 5, 7, 3, 2];
-        assert_eq!(&expected,
-                   &build_prefix_array(&haplotypes, n_haplo, n_sites));
+        let haplotypes: Vec<u8> = vec![0, 1, 1, 0, 0, 1, 1, 0,
+                                       1, 1, 1, 1, 0, 0, 1, 1,
+                                       0, 0, 1, 1, 0, 0, 0, 0,
+                                       1, 0, 1, 1, 0, 0, 0, 1,
+                                       0, 0, 1, 1, 0, 1, 0, 1,
+                                       1, 1, 1, 0, 0, 0, 1, 0];
+        let expected_1: Vec<usize> = vec![0, 1, 2, 3, 4, 5, 6, 7];
+        assert_eq!(&expected_1,
+                   &build_positional_prefix_array(&haplotypes, n_haplo, 1));
+        let expected_6: Vec<usize> = vec![4, 1, 6, 0, 5, 7, 3, 2];
+        assert_eq!(&expected_6,
+                   &build_positional_prefix_array(&haplotypes, n_haplo, n_sites));
     }
 }
